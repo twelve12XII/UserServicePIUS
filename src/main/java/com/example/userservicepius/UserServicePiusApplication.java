d8e177694f62f50e9e2f3052938c665769e150a0
twelve12XII <irishka.nikitina2002@mail.ru>
2024-04-11 15:19:04 +0300

    Initial commit

---
+++ src/main/java/com/example/userservicepius/UserServicePiusApplication.java
@@ -0,0 +1,13 @@
+package com.example.userservicepius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicePiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServicePiusApplication.class, args);
+    }
+
+}
